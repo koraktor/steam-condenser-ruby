cc297a3b60b17c81c421ff3d3463dbee06082b45
Sebastian Staudt <koraktor@gmail.com>
2012-04-29 12:51:45 +0200

    Namespaced XMLData

--- lib/steam/community/xml_data.rb
+++ lib/steam/community/xml_data.rb
@@ -7,21 +7,24 @@ require 'open-uri'
 
 require 'multi_xml'
 
-# This class provides basic functionality to parse XML data
-#
-# @author Sebastian Staudt
-module XMLData
+module SteamCondenser
 
-  # Parse the given URL as XML data using `multi_xml`
+  # This class provides basic functionality to parse XML data
   #
-  # @param [String] url The URL to parse
-  # @return [Hash<String, Object>] The data parsed from the XML document
-  # @raise [SteamCondenserError] if an error occurs while parsing the XML data
-  def parse(url)
-    data = open(url, { :proxy => true })
-    @xml_data = MultiXml.parse(data).values.first
-  rescue
-    raise SteamCondenserError.new "XML data could not be parsed: #{$!.message}", $!
-  end
+  # @author Sebastian Staudt
+  module XMLData
 
+    # Parse the given URL as XML data using `multi_xml`
+    #
+    # @param [String] url The URL to parse
+    # @return [Hash<String, Object>] The data parsed from the XML document
+    # @raise [SteamCondenserError] if an error occurs while parsing the XML data
+    def parse(url)
+      data = open(url, { :proxy => true })
+      @xml_data = MultiXml.parse(data).values.first
+    rescue
+      raise SteamCondenserError.new "XML data could not be parsed: #{$!.message}", $!
+    end
+
+  end
 end
