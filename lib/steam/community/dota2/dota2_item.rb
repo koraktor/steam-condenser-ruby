b51dbb61a8148b52b15c875511637a5a1aa184d1
Sebastian Staudt <koraktor@gmail.com>
2012-07-15 12:27:58 +0200

    Added a getter for the equipped status of DotA2Item

--- lib/steam/community/dota2/dota2_item.rb
+++ lib/steam/community/dota2/dota2_item.rb
@@ -5,20 +5,29 @@
 
 require 'steam/community/game_item'
 
-# Represents a Portal 2 item
+# Represents a DotA 2 item
 #
 # @author Sebastian Staudt
 class Dota2Item
 
   include GameItem
 
-  # Creates a new instance of a Dota 2 item with the given data
+  # Creates a new instance of a DotA 2 item with the given data
   #
   # @param [Dota2Inventory] inventory The inventory this item is contained in
   # @param [Hash<Symbol, Object>] item_data The data specifying this item
   # @raise [WebApiError] on Web API errors
   def initialize(inventory, item_data)
     super
+
+    @equipped = !item_data[:equipped].nil?
+  end
+
+  # Returns whether this item is equipped by this player at all
+  #
+  # @return [Boolean] Whether this item is equipped by this player at all
+  def equipped?
+    @equipped
   end
 
 end
