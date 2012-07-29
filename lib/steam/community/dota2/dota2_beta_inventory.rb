bb01a5e53e9fb346c6ec12b3f0b21c5e3ff2b8f2
Sebastian Staudt <koraktor@gmail.com>
2012-07-29 16:27:37 +0200

    Moved DotA2 inventory files into the SteamCondenser namespace

--- lib/steam/community/dota2/dota2_beta_inventory.rb
+++ lib/steam/community/dota2/dota2_beta_inventory.rb
@@ -6,25 +6,29 @@
 require 'steam/community/dota2/dota2_item'
 require 'steam/community/game_inventory'
 
-# Represents the inventory of a player of the DotA 2 beta
-#
-# @author Sebastian Staudt
-class Dota2BetaInventory < GameInventory
+module SteamCondenser
 
-  # The Steam application ID of the DotA 2 beta
-  APP_ID = 205790
-
-  # Creates a new inventory object for the given SteamID64 in the DotA 2 beta
-  # (App ID 205790)
+  # Represents the inventory of a player of the DotA 2 beta
   #
-  # @param [Fixnum] steam_id64 The 64bit SteamID of the player to get the
-  #        inventory for
-  # @macro cacheable
-  def initialize(steam_id64)
-    super APP_ID, steam_id64
-  end
+  # @author Sebastian Staudt
+  class Dota2BetaInventory < GameInventory
 
-  # The class representing DotA 2 items
-  @@item_class = Dota2Item
+    # The Steam application ID of the DotA 2 beta
+    APP_ID = 205790
+
+    # Creates a new inventory object for the given SteamID64 in the DotA 2 beta
+    # (App ID 205790)
+    #
+    # @param [Fixnum] steam_id64 The 64bit SteamID of the player to get the
+    #        inventory for
+    # @macro cacheable
+    def initialize(steam_id64)
+      super APP_ID, steam_id64
+    end
+
+    # The class representing DotA 2 items
+    @@item_class = Dota2Item
+
+  end
 
 end
--- lib/steam/community/dota2/dota2_item.rb
+++ lib/steam/community/dota2/dota2_item.rb
@@ -5,29 +5,33 @@
 
 require 'steam/community/game_item'
 
-# Represents a DotA 2 item
-#
-# @author Sebastian Staudt
-class Dota2Item
+module SteamCondenser
 
-  include GameItem
-
-  # Creates a new instance of a DotA 2 item with the given data
+  # Represents a DotA 2 item
   #
-  # @param [Dota2Inventory] inventory The inventory this item is contained in
-  # @param [Hash<Symbol, Object>] item_data The data specifying this item
-  # @raise [WebApiError] on Web API errors
-  def initialize(inventory, item_data)
-    super
+  # @author Sebastian Staudt
+  class Dota2Item
 
-    @equipped = !item_data[:equipped].nil?
-  end
+    include GameItem
 
-  # Returns whether this item is equipped by this player at all
-  #
-  # @return [Boolean] Whether this item is equipped by this player at all
-  def equipped?
-    @equipped
+    # Creates a new instance of a DotA 2 item with the given data
+    #
+    # @param [Dota2Inventory] inventory The inventory this item is contained in
+    # @param [Hash<Symbol, Object>] item_data The data specifying this item
+    # @raise [WebApiError] on Web API errors
+    def initialize(inventory, item_data)
+      super
+
+      @equipped = !item_data[:equipped].nil?
+    end
+
+    # Returns whether this item is equipped by this player at all
+    #
+    # @return [Boolean] Whether this item is equipped by this player at all
+    def equipped?
+      @equipped
+    end
+
   end
 
 end
