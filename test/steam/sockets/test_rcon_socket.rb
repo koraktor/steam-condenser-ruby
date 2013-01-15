70d8f1bc16907b59afd4b5c9be70f1a48efa15c4
Sebastian Staudt <koraktor@gmail.com>
2013-01-15 07:25:24 +0100

    Added test for reset RCON connection

--- test/steam/sockets/test_rcon_socket.rb
+++ test/steam/sockets/test_rcon_socket.rb
@@ -96,7 +96,15 @@ class TestRCONSocket < Test::Unit::TestCase
       @socket.reply
     end
 
-    should 'raise an error if the client has been banned' do
+    should 'raise an RCONBanError if the client has been banned' do
+      @socket.expects(:receive_packet).with(4).raises Errno::ECONNRESET
+
+      assert_raise RCONBanError do
+        assert_nil @socket.reply
+      end
+    end
+
+    should 'raise an RCONNoAuthError if the connection has been dropped' do
       @socket.expects(:receive_packet).with(4).returns 0
       @tcp_socket.expects :close
 
