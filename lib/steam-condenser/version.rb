0f7eaa32e3d1dcf9babf842c65a08f4ab32b8c98
Sebastian Staudt <koraktor@gmail.com>
2013-01-20 13:07:26 +0100

    Version bump to 1.3.1

--- lib/steam-condenser/version.rb
+++ lib/steam-condenser/version.rb
@@ -1,11 +1,11 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2010-2012, Sebastian Staudt
+# Copyright (c) 2010-2013, Sebastian Staudt
 
 module SteamCondenser
 
   # The current version of Steam Condenser
-  VERSION = '1.3.0'
+  VERSION = '1.3.1'
 
 end
