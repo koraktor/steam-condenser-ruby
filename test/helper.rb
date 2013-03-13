4a59a1616d0c81fa9d9b9e17f7dc155d3376682a
Sebastian Staudt <koraktor@gmail.com>
2013-03-13 23:52:08 +0100

    Added configuration for Coveralls

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,10 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2011-2012, Sebastian Staudt
+# Copyright (c) 2011-2013, Sebastian Staudt
+
+require 'coveralls'
+Coveralls.wear!
 
 require 'test/unit'
 
